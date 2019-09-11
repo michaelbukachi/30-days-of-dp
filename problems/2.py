45257025f1f0f01ac37ed5b6a9bfeb4ff87fa16a
Michael Bukachi <michaelbukachi@gmail.com>
2019-09-11 23:30:43 +0300

    feat: day 2

---
+++ problems/2.py
@@ -0,0 +1,18 @@
+"""
+Maximum sum of subarray implementation
+"""
+
+
+def max_sum(array):
+    cur_sum = global_sum = array[0]
+    for i in array[1:]:
+        cur_sum = max(i, cur_sum + i)
+        global_sum = max(cur_sum, global_sum)
+
+    return global_sum
+
+
+if __name__ == '__main__':
+    assert max_sum([2, -9, 5, 1, -4, 6, 0, -7, 8]) == 9
+    assert max_sum([1, 5, -1, 0, 10]) == 15
+    assert max_sum([0, -1, -5, 0, -4]) == 0
