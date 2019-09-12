0f6307622c2a6cd62e26f72eee61883dc5d61496
Michael Bukachi <michaelbukachi@gmail.com>
2019-09-12 22:47:23 +0300

    feat: day 2, add all negative numbers test

--- problems/2.py
+++ problems/2.py
@@ -16,3 +16,4 @@ if __name__ == '__main__':
     assert max_sum([2, -9, 5, 1, -4, 6, 0, -7, 8]) == 9
     assert max_sum([1, 5, -1, 0, 10]) == 15
     assert max_sum([0, -1, -5, 0, -4]) == 0
+    assert max_sum([-4, -3, -1, -9, -10]) == -1
